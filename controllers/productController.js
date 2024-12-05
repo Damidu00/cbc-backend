7e778206e1f2447a1926a75c15e322c631a92236
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2024-12-05 14:37:23 +0530

    product's two functions done with routes😮‍💨

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,40 @@
 import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
 
+export async function createProduct(req,res){
 
+    if(!isAdmin(req)){
+        res.json({
+            message : "Login as admin to create a product"
+        })
+        return
+    }
+
+    const newProductData = req.body
+    const product = new Product(newProductData)
+
+    try {
+        await product.save();
+        res.json({
+            message : "product Created successfully"
+        })
+    } catch (error) {
+        res.json({
+            message : "product not created",
+            error : error.message
+        })
+        
+    }
+}
+
+export async function getProducts(req,res){
+   try {
+    const products = await Product.find();
+    res.json(products)
+   } catch (error) {
+    res.json({
+        message : "cannot get products",
+        error : error.message
+    })
+   } 
+}
