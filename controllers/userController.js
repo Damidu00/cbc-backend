2fdd94487c069cc8776c8760a6d8be755113e1a9
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2024-12-05 13:09:23 +0530

    create admin and customer validation functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -90,3 +90,26 @@ export async function loginUser(req, res) {
     }
 }
 
+
+export function isAdmin(req){
+    if(req.user == null){
+        return false
+    }
+
+    if(req.user.type != "admin"){
+        return false
+    }
+    return true
+}
+
+export function isCustomer(req){
+    if(req.user == null){
+        return false
+    }
+    if(req.user.type != "customer"){
+        return false
+    }
+
+    return false
+}
+
