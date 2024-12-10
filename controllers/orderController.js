f7a774a927c797e2d05865b88eb005e285ffb264
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2024-12-10 12:19:29 +0530

    create order issue done!

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -21,12 +21,13 @@ export async function createOrder(req,res){
             const currentOrderId = latestOrder[0].orderId
             const numberString = currentOrderId.replace("CBC","")
             const number = parseInt (numberString)
-            const newNumber = (number + 1).toString().padStart(4, "0");
+            const newNumber = (number + 1).toString().padStart(4,"0");
             orderId = "CBC" + newNumber
         }
 
         const newOrderData = req.body
         newOrderData.orderId = orderId
+        newOrderData.email = req.user.email
 
         const order = new Order(newOrderData)
 
@@ -46,8 +47,8 @@ export async function createOrder(req,res){
 export async function getOrders(req,res){
     try {
         const orders = await Order.find({email : req.user.email})
-
-        req.json(orders)
+        console.log(req.user)
+        res.json(orders)
 
     } catch (error) {
         res.status(500).json({
