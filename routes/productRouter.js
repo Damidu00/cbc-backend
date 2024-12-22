e4b8788e76f543d4d599475b08ba6c71a882bb85
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2024-12-22 23:00:33 +0530

    getProductById Router Created ✨

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { createProduct, deleteProduct, getProducts } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getProductById, getProducts } from "../controllers/productController.js";
 
 const productRouter = express.Router()
 
 productRouter.post("/",createProduct)
 productRouter.get("/",getProducts)
 productRouter.delete("/:id",deleteProduct)
+productRouter.get("/:id",getProductById)
 
 export default productRouter
