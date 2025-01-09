e422818f93b199f47e13b6ae496bf7a3191d9f4b
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-01-09 23:19:32 +0530

    tested and bugs fixed in create message part

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use((req,res,next)=>{
 app.use("/api/users",userRouter)
 app.use("/api/products",productRouter)
 app.use("/api/orders",orderRouter)
-app.use("/api/contactUs",contactUsRouter)
+app.use("/api/contactus",contactUsRouter)
 
 app.listen(5000,() =>{
     console.log('Server is running on port 5000')
