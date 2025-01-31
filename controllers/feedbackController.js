c55e676992873741fdd56b58dfae33f4df633efa
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-01-31 23:25:04 +0530

    create feedback function created✨

---
+++ controllers/feedbackController.js
@@ -0,0 +1,25 @@
+import Feedback from "../models/feedback";
+
+export async function createFeedback(req,res){
+    try {
+        const {userName , message} = req.body;
+
+        const newFeedback = new Feedback({
+            userName,
+            message
+        });
+
+        await newFeedback.save()
+
+        res.status(201).json({
+            message : "Feedback created succsessfully",
+            feedback : newFeedback
+        })
+
+    } catch (error) {
+        res.json({
+            error : error.message,
+            message : "Cannot send feedback"
+        })
+    }
+}
