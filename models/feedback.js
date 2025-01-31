fc13511c3924935d7e6352e2690ebd9f92ca001b
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-01-31 23:21:48 +0530

    added some changes for feedback model

--- models/feedback.js
+++ models/feedback.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
 const FeedbackShema = mongoose.Schema({
+    feedbackId : {
+        type : String,
+        required : true,
+        unique: true
+    },
     userName : {
         type : String,
         required : true
