3708e34cdbd8b376b81ffa7f4d4c89a3c7932b12
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-02-01 00:09:22 +0530

    added router for getAllfeedbacks

--- routes/feedbackRouter.js
+++ routes/feedbackRouter.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createFeedback } from '../controllers/feedbackController.js'
+import { createFeedback, getAllFeedbacks } from '../controllers/feedbackController.js'
 
 
 const feedbackRouter = express.Router()
 
 feedbackRouter.post("/",createFeedback)
+feedbackRouter.get("/",getAllFeedbacks)
 
 
 export default feedbackRouter
