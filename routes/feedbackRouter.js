822288da2c3305061b4d2453876ec0bef70998d9
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-02-01 00:28:54 +0530

    added router for admin reply function✨

--- routes/feedbackRouter.js
+++ routes/feedbackRouter.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import { createFeedback, getAllFeedbacks } from '../controllers/feedbackController.js'
+import { adminReply, createFeedback, getAllFeedbacks } from '../controllers/feedbackController.js'
 
 
 const feedbackRouter = express.Router()
 
 feedbackRouter.post("/",createFeedback)
 feedbackRouter.get("/",getAllFeedbacks)
+feedbackRouter.post("/reply",adminReply)
 
 
 export default feedbackRouter
