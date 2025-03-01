442c5b197189924d069fa545ad2416d7a5ca89a8
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-03-01 18:22:12 +0530

    search products function done

--- controllers/productController.js
+++ controllers/productController.js
@@ -118,3 +118,21 @@ export async function updateProduct(req,res){
         })
     }
 }
+
+export async function searchProducts(req, res) {
+    const query = req.params.query;
+    try {
+      const products = await Product.find({
+        $or: [
+          { productName: { $regex: query, $options: "i" } },
+          { altNames: { $elemMatch: { $regex: query, $options: "i" } } },
+        ],
+      });
+  
+      res.json(products);
+    } catch (e) {
+      res.status(500).json({
+        e,
+      });
+    }
+  }
