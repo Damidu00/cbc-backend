329a9b44cae9aa1d627468a761b01ef693fa51fc
Damidu00 <154224349+Damidu00@users.noreply.github.com>
2025-03-01 23:35:42 +0530

    get user routes added

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,14 @@
 import express from 'express'
-import { createUser, deleteUserById, getAllUsers, googleLogin, loginUser } from '../controllers/userController.js'
+import { createUser, deleteUserById, getAllUsers, getUser, googleLogin, loginUser } from '../controllers/userController.js'
 
 const userRouter = express.Router()
 
+userRouter.get("/",getUser)
 userRouter.post("/",createUser)
 userRouter.post("/login",loginUser)
 userRouter.delete("/:id",deleteUserById)
 userRouter.get("/",getAllUsers)
 userRouter.post("/google",googleLogin)
+
 
 export default userRouter
